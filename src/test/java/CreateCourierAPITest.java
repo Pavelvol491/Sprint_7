eb7d395d25043b9cba3800c0d6cbe1618c12b926
Pavelvol491 <138298958+Pavelvol491@users.noreply.github.com>
2023-10-13 18:03:44 +0400

    improved with notice second time

--- src/test/java/CreateCourierAPITest.java
+++ src/test/java/CreateCourierAPITest.java
@@ -3,111 +3,107 @@ import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import com.github.javafaker.Faker;
 import com.google.gson.Gson;
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-class Courier {
-    private String login;
-    private String password;
+import java.util.Arrays;
+import java.util.Collection;
 
-    public Courier(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-}
-
+@RunWith(Parameterized.class)
 public class CreateCourierAPITest {
 
     private static final String BASE_URL = "http://qa-scooter.praktikum-services.ru";
     private static final String COURIER_LOGIN_ENDPOINT = "/api/v1/courier/login";
     private static final String COURIER_CREATE_ENDPOINT = "/api/v1/courier";
 
-    private Faker faker = new Faker();
+    private static Faker faker = new Faker();
     private Gson gson = new Gson();
+    private Response loginResponse;
 
     @BeforeClass
     public static void setUp() {
         RestAssured.baseURI = BASE_URL;
     }
 
-    @Test
-    @DisplayName("1. Тест логина курьера с правильными данными")
-    @Description("Тест проверяет успешный логин курьера с правильными данными")
-    public void testLoginCourierWithValidData() {
-        String login = "ninja";
-        String password = "1234";
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"ninja", "1234"}, // Правильные данные для логина
+                {"", ""}, // Отсутствуют данные для логина
+                {faker.name().username(), faker.internet().password()} // Данные для создания курьера
+        });
+    }
 
-        Response response = loginCourier(new Courier(login, password));
+    private String login;
+    private String password;
 
-        assertEquals(200, response.getStatusCode());
-        assertTrue(response.getBody().jsonPath().getInt("id") > 0);
+    public CreateCourierAPITest(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Before
+    public void createTestData() {
+        // Код для создания тестовых данных
+        if (login.isEmpty() || password.isEmpty()) {
+            loginResponse = null; // В случае отсутствия данных, не выполняем логин
+        } else {
+            loginResponse = loginCourier(new Courier(login, password));
+        }
+    }
+
+    @After
+    public void deleteTestData() {
+
     }
 
     @Test
-    @DisplayName("2. Тест логина курьера с отсутствующими данными")
-    @Description("Тест проверяет возврат ошибки при логине без логина или пароля")
-    public void testLoginCourierWithMissingData() {
-        Response response = loginCourier(new Courier("", ""));
-
-        assertEquals(400, response.getStatusCode());
-        assertEquals("Недостаточно данных для входа", response.getBody().jsonPath().getString("message"));
+    @DisplayName("Тест логина курьера")
+    @Description("Тест логина курьера с разными данными")
+    public void testLoginCourier() {
+        if (loginResponse == null) {
+            assertEquals(400, loginResponse.getStatusCode());
+            assertEquals("Недостаточно данных для входа", loginResponse.getBody().jsonPath().getString("message"));
+        } else {
+            assertEquals(200, loginResponse.getStatusCode());
+            assertTrue(loginResponse.getBody().jsonPath().getInt("id") > 0);
+        }
     }
 
     @Test
-    @DisplayName("3. Тест создания курьера с правильными данными")
-    @Description("Тест проверяет успешное создание курьера с правильными данными")
-    public void testCreateCourierWithValidData() {
-        String login = faker.name().username();
-        String password = faker.internet().password();
-
+    @DisplayName("Тест создания курьера")
+    @Description("Тест создания курьера с разными данными")
+    public void testCreateCourier() {
         Response response = createCourier(new Courier(login, password));
 
-        assertEquals(201, response.getStatusCode());
-        assertTrue(response.getBody().jsonPath().getBoolean("ok"));
-    }
-
-    @Test
-    @DisplayName("4. Тест создания курьера с отсутствующими данными")
-    @Description("Тест проверяет возврат ошибки при создании курьера без обязательных данных")
-    public void testCreateCourierWithMissingData() {
-        Response response = createCourier(new Courier("", ""));
-
-        assertEquals(400, response.getStatusCode());
-        assertEquals("Недостаточно данных для создания учетной записи", response.getBody().jsonPath().getString("message"));
-    }
-
-    @Test
-    @DisplayName("5. Тест создания курьера с существующим логином")
-    @Description("Тест проверяет возврат ошибки при создании курьера с существующим логином")
-    public void testCreateDuplicateCourier() {
-        String login = faker.name().username();
-        String password = faker.internet().password();
-
-        createCourier(new Courier(login, password)); // Создаем курьера с теми же данными
-
-        Response response = createCourier(new Courier(login, password)); // Пытаемся создать курьера с теми же данными снова
-
-        assertEquals(409, response.getStatusCode());
-        assertEquals("Этот логин уже используется. Попробуйте другой.", response.getBody().jsonPath().getString("message"));
+        if (login.isEmpty() || password.isEmpty()) {
+            assertEquals(400, response.getStatusCode());
+            assertEquals("Недостаточно данных для создания учетной записи", response.getBody().jsonPath().getString("message"));
+        } else {
+            assertEquals(201, response.getStatusCode());
+            assertTrue(response.getBody().jsonPath().getBoolean("ok"));
+        }
     }
 
     private Response loginCourier(Courier courier) {
-        String requestBody = gson.toJson(courier);
         return RestAssured.given()
                 .contentType(ContentType.JSON)
-                .body(requestBody)
+                .body(courier)
                 .post(COURIER_LOGIN_ENDPOINT);
     }
 
     private Response createCourier(Courier courier) {
-        String requestBody = gson.toJson(courier);
         return RestAssured.given()
                 .contentType(ContentType.JSON)
-                .body(requestBody)
+                .body(courier)
                 .post(COURIER_CREATE_ENDPOINT);
     }
 }
